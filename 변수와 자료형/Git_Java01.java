57a86840006de249042b8647e698ddc00cc6a044
alswo471 <alswo471@naver.com>
2022-08-21 17:45:27 +0900

    08/21

---
+++ 변수와 자료형/Git_Java01.java
@@ -0,0 +1,9 @@
+package study01;
+
+public class Git_Java01 {
+	public static void main(String[] args) {
+		int num; //int 는 자료명 num 은 변수이름 , 정수형 변수 num선언 
+		num = 5; // 5을 num 변수에 대입
+		System.out.println(num);
+	}
+}
---
+++ 변수와 자료형/Git_Java02.java
@@ -0,0 +1,17 @@
+package study01;
+
+public class Git_Java02 {
+	public static void main(String[] args) {
+		char alphabet1 = 'A';
+		System.out.println(alphabet1); // 문자 출력
+		System.out.println((int)alphabet1); // 문자에 해당하는 정수 값(아스키 코드 값)출력 
+		
+		char alphabet2 = 66; // 정수값 대입
+		System.out.println(alphabet2); // 정수 값에 해당하는 문자 출력
+		
+		int alphabet3 = 67;
+		System.out.println(alphabet3); // 문자 정수 값 출력
+		System.out.println((char)alphabet3); // 정수 값에 해당하는 문자 출력
+		
+	}
+}
