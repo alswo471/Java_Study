9e8d82afd91eb6b966d7bb707359c901a1cfbb63
alswo471 <alswo471@naver.com>
2022-08-23 21:16:52 +0900

    08/23

---
+++ 변수와 자료형/Git_Java03.java
@@ -0,0 +1,12 @@
+package study01;
+
+public class Git_Java03 {
+	public static void main(String[] args) {
+		
+			char kor1 = 'วั';
+			char kor2 = '\uD55C';
+			
+			System.out.println(kor1);
+			System.out.println(kor2);
+		}
+}
---
+++ 변수와 자료형/Git_Java04.java
@@ -0,0 +1,15 @@
+package study01;
+
+public class Git_Java04 {
+	public static void main(String[] args) {
+		
+		int a = 70;
+		int b = -70; // 문자형 변수에 음수를 넣으면 오류
+		
+		char a2 = 70;
+		
+		System.out.println((char)a);
+		System.out.println((char)b);
+		System.out.println(a2);
+	}
+}
